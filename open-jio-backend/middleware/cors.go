dbfc0c959905f3904e781b9892724d8689487a3b
unknown <mingxiang130@gmail.com>
2024-05-30 22:19:56 +0800

    updated CORS
    updated CORS to allow frontend

--- open-jio-backend/middleware/cors.go
+++ open-jio-backend/middleware/cors.go
@@ -1,11 +1,13 @@
 package middleware
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func CORSMiddleware(c *gin.Context) {
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:3001")
+	c.Writer.Header().Set("Access-Control-Allow-Origin", os.Getenv("FRONTEND_URL"))
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
