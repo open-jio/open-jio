a8fe08f3278133f0f2c9c07ef8efa6c337313389
unknown <mingxiang130@gmail.com>
2024-05-31 16:11:07 +0800

    fixed bug

--- open-jio-frontend/src/pages/Emailverifiedpage.tsx
+++ open-jio-frontend/src/pages/Emailverifiedpage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Emailverifiedpage = () => {
   const [searchParams] = useSearchParams();
