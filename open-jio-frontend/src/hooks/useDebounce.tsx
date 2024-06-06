da298d1b763b4d1bcad0a149b52930923de07eb6
rachelyeohm <rachelyeohm@gmail.com>
2024-06-07 04:54:32 +0800

    Fixed debounce, did search bar styling, fixed bugs

---
+++ open-jio-frontend/src/hooks/useDebounce.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef, useMemo } from "react";
+import debounce from 'lodash/debounce';
+
+
+
+const useDebounce = (callback : () => void) => {
+    const ref = useRef<(() => void) | null>(null);
+  
+    useEffect(() => {
+      ref.current = callback;
+    }, [callback]);
+  
+    const debouncedCallback = useMemo(() => {
+      const func = () => {
+        ref.current?.();
+      };
+  
+      return debounce(func, 500);
+    }, []);
+  
+    return debouncedCallback;
+  };
+
+export default useDebounce;
