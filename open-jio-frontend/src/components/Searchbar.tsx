ca3aa2c8355596aada95eff98d2062f370b480b8
rachelyeohm <rachelyeohm@gmail.com>
2024-06-07 04:55:00 +0800

    Removed unnecessary imports

--- open-jio-frontend/src/components/Searchbar.tsx
+++ open-jio-frontend/src/components/Searchbar.tsx
@@ -1,4 +1,4 @@
-import {useState, useRef} from "react"
+import {useState} from "react"
 import {useNavigate} from "react-router-dom";
 import {Input} from "antd";
 import { Event } from "../types/event";
